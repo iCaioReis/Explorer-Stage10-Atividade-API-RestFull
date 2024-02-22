e1ddd9f7c5295eff030e5035db7f33cc9ff5095a
iCaioReis <caioreis3186@gmail.com>
2024-02-22 07:05:20 -0300

    Validating registered user

--- src/routes/sessions.routes.js
+++ src/routes/sessions.routes.js
@@ -1,4 +1,4 @@
-import { Router } from "express";
+const { Router } = require ("express");
 
 const SessionsController = require("../controllers/SessionsController");
 const sessionsController = new SessionsController();
