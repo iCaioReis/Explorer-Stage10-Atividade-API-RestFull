0b9008fac9302560bc811644c442381a14cf72f6
iCaioReis <caioreis3186@gmail.com>
2024-04-06 20:50:59 -0300

    User repository

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -7,10 +7,6 @@ class usersController {
     async create(req, res) {
         const { name, email, password } = req.body;
 
-        const database = await sqliteConnection();
-
-        const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
-
         if (checkUserExists) {
             throw new AppError("E-mail já cadastrado!");
         }
